fa25d899d6253232d3e2592962691d9c0b60dd94
unknown <namannjaj456@gmai.com>
2021-02-08 03:47:42 +0530

    First Commit

---
+++ Assets/Scripts/DisablePanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisablePanel : MonoBehaviour
+{
+    public GameObject gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void ClosePanel()
+    {
+        gameObject.transform.gameObject.SetActive(false);
+    }
+}
